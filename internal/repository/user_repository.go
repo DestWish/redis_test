d7be28e2f8ed0501d5ac1ec9bbd614613e1c070d
Oleg_Markov (DxstW1sh) <gkdestroy@yandex.ru>
2026-04-24 23:33:31 +0200

    add err 'response', also add func for converting id to redis key

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -3,7 +3,6 @@ package repository
 import (
 	"context"
 	"fmt"
-	"strconv"
 
 	"github.com/DestWish/redis_test/internal/models"
 	"github.com/redis/go-redis/v9"
@@ -11,18 +10,17 @@ import (
 )
 
 type UserRepository struct {
-	db *gorm.DB
+	db          *gorm.DB
 	redisClient *redis.Client
 }
 
-func New_userRepo (db *gorm.DB, redisClient *redis.Client) *UserRepository {
- return &UserRepository{db: db, redisClient: redisClient}
+func New_userRepo(db *gorm.DB, redisClient *redis.Client) *UserRepository {
+	return &UserRepository{db: db, redisClient: redisClient}
 }
 
 func (r *UserRepository) userCaching(ctx context.Context, user *models.User) error {
-	key := strconv.Itoa(int(user.ID))
+	key := userCacheKey(user.ID)
 	if err := r.redisClient.HSet(ctx, key, user).Err(); err != nil {
-		fmt.Printf("Ошибка кэширования: %v\n", err)
 		return err
 	}
 
@@ -36,28 +34,34 @@ func (r *UserRepository) Create(ctx context.Context, user *models.User) (uint, e
 		return user.ID, err
 	}
 
-	r.userCaching(ctx, user)
+	if err := r.userCaching(ctx, user); err != nil {
+		return user.ID, err
+	}
 
 	return user.ID, nil
 }
 
 func (r *UserRepository) GetUser(ctx context.Context, userID uint) (models.User, error) {
-	key := strconv.Itoa(int(userID))
+	key := userCacheKey(userID)
 	var user models.User
 
 	err := r.redisClient.HGetAll(ctx, key).Scan(&user)
-	if err == nil && user.ID != 0{
+	if err == nil && user.ID != 0 {
 		return user, nil
 	}
-	
+
 	if err := r.db.Where("id = ?", userID).First(&user).Error; err != nil {
 		fmt.Printf("Данного пользователя не существует %v\n", err.Error())
 		return user, err
-	} 
-	
+	}
+
 	if err := r.userCaching(ctx, &user); err != nil {
 		fmt.Printf("Ошибка кэширования %v\n", err.Error())
 	}
 
 	return user, nil
+}
+
+func userCacheKey(userID uint) string {
+	return fmt.Sprintf("user:%v", userID)
 }
