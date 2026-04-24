f590035c0bda60271ccfa5cf92501193e3dc9e84
Oleg_Markov (DxstW1sh) <gkdestroy@yandex.ru>
2026-04-24 15:22:24 +0200

    Реализовал механизмы кжширования в пакете репозитория

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -1,33 +1,62 @@
 package repository
 
 import (
+	"context"
 	"fmt"
+	"strconv"
 
 	"github.com/DestWish/redis_test/internal/models"
+	"github.com/redis/go-redis/v9"
 	"gorm.io/gorm"
 )
 
 type UserRepository struct {
 	db *gorm.DB
+	redisClient *redis.Client
 }
 
-func New_userRepo (db *gorm.DB) *UserRepository {
- return &UserRepository{db: db}
+func New_userRepo (db *gorm.DB, redisClient *redis.Client) *UserRepository {
+ return &UserRepository{db: db, redisClient: redisClient}
 }
 
-func (r *UserRepository) Create(user *models.User) (*models.User, error) {
+func (r *UserRepository) userCaching(ctx context.Context, user *models.User) error {
+	key := strconv.Itoa(user.ID)
+	if err := r.redisClient.HSet(ctx, key, user).Err(); err != nil {
+		fmt.Printf("Ошибка кэширования: %v\n", err)
+		return err
+	}
+
+	return nil
+}
+
+func (r *UserRepository) Create(ctx context.Context, user *models.User) (int, error) {
 	result := r.db.Create(user)
 	if err := result.Error; err != nil {
 		fmt.Printf("Error %v", err)
-		return nil, err
+		return user.ID, err
 	}
-	return user, nil
+
+	r.userCaching(ctx, user)
+
+	return user.ID, nil
 }
 
-func (r *UserRepository) GetUser(userID uint) (models.User, error) {
+func (r *UserRepository) GetUser(ctx context.Context, userID uint) (models.User, error) {
+	key := strconv.Itoa(int(userID))
 	var user models.User
-	if err := r.db.Where("id = ?", userID).Find(&user).Error; err != nil {
-		return models.User{}, err
+
+	err := r.redisClient.HGetAll(ctx, key).Scan(&user)
+	if err == nil && user.ID != 0{
+		return user, nil
+	}
+	
+	if err := r.db.Where("id = ?", userID).First(&user).Error; err != nil {
+		fmt.Printf("Данного пользователя не существует %v\n", err.Error())
+		return user, err
+	} 
+	
+	if err := r.userCaching(ctx, &user); err != nil {
+		fmt.Printf("Ошибка кэширования %v\n", err.Error())
 	}
 
 	return user, nil
