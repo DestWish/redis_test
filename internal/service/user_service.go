d7a4ee774a4ba4c6ee425601d590bac9a5519340
Oleg_Markov (DxstW1sh) <gkdestroy@yandex.ru>
2026-04-26 14:16:10 +0200

    remove errors handling from service layer

--- internal/service/user_service.go
+++ internal/service/user_service.go
@@ -2,7 +2,6 @@ package service
 
 import (
 	"context"
-	"fmt"
 
 	"github.com/DestWish/redis_test/internal/models"
 	"github.com/DestWish/redis_test/internal/repository"
@@ -16,49 +15,22 @@ func NewUserService(repo *repository.UserRepository) *User_service {
 	return &User_service{Repo: repo}
 }
 
-func (s *User_service) CreateUser(ctx context.Context, req *models.CreateUserRequest) uint {
-	ID, err := s.Repo.Create(ctx, req)
-	if err != nil {
-		fmt.Printf("Error: %v", err)
-	}
-	return ID
+func (s *User_service) CreateUser(ctx context.Context, req *models.CreateUserRequest) (uint, error) {
+	return s.Repo.Create(ctx, req)
 }
 
-func (s *User_service) ReadUser(ctx context.Context, req *models.ReadUserRequest) models.User {
-	user, err := s.Repo.GetUser(ctx, req)
-	if err != nil {
-		fmt.Printf("Error: %v", err)
-	}
-	return user
+func (s *User_service) ReadUser(ctx context.Context, req *models.ReadUserRequest) (models.User, error) {
+	return s.Repo.GetUser(ctx, req)
 }
 
-func (s *User_service) ReplaceUser(ctx context.Context, req *models.UpdateUserRequest) bool{
-	ok, err := s.Repo.ReplaceUser(ctx, req)
-	if err != nil {
-		fmt.Printf("Error: %v", err)
-		return ok
-	}
-
-	return ok
+func (s *User_service) ReplaceUser(ctx context.Context, req *models.UpdateUserRequest) (bool, error) {
+	return s.Repo.ReplaceUser(ctx, req)
 }
 
-func (s *User_service) PatchUser(ctx context.Context, req *models.PatchUserRequest) bool{
-	ok, err := s.Repo.PatchUser(ctx, req)
-	if err != nil {
-		fmt.Printf("Error: %v", err)
-		return ok
-	}
-
-	return ok
+func (s *User_service) PatchUser(ctx context.Context, req *models.PatchUserRequest) (bool, error) {
+	return s.Repo.PatchUser(ctx, req)
 }
 
-
-func (s *User_service) DeleteUser(ctx context.Context, req *models.DeleteUserRequest) bool {
-	ok, err := s.Repo.Delete(ctx, req)
-	if err != nil {
-		fmt.Printf("Error: %v", err)
-		return ok
-	}
-
-	return ok
+func (s *User_service) DeleteUser(ctx context.Context, req *models.DeleteUserRequest) (bool, error) {
+	return s.Repo.Delete(ctx, req)
 }
