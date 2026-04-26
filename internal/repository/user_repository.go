5697affbd3cead2376b1e8dba86560226c021cce
Oleg_Markov (DxstW1sh) <gkdestroy@yandex.ru>
2026-04-26 14:12:12 +0200

    fix errors handling on repo layer

--- internal/repository/user_repository.go
+++ internal/repository/user_repository.go
@@ -21,7 +21,7 @@ func NewUserRepo(db *gorm.DB, redisClient *redis.Client) *UserRepository {
 func (r *UserRepository) userCaching(ctx context.Context, user *models.User) error {
 	key := userCacheKey(user.ID)
 	if err := r.redisClient.HSet(ctx, key, user).Err(); err != nil {
-		return err
+		return fmt.Errorf("Repo: Cache failed: %w", err)
 	}
 
 	return nil
@@ -29,17 +29,11 @@ func (r *UserRepository) userCaching(ctx context.Context, user *models.User) err
 
 func (r *UserRepository) Create(ctx context.Context, req *models.CreateUserRequest) (uint, error) {
 	user := &models.User{Name: req.Name, Email: req.Email}
-	result := r.db.Model(&models.User{}).Create(user)
-	if err := result.Error; err != nil {
-		fmt.Printf("Error %v", err)
-		return user.ID, err
+	if err := r.db.Model(&models.User{}).Create(user).Error; err != nil {
+		return 0, fmt.Errorf("Repo: Create user failed: %w", err)
 	}
 
-	if err := r.userCaching(ctx, user); err != nil {
-		return user.ID, err
-	}
-
-	return user.ID, nil
+	return user.ID, r.userCaching(ctx, user)
 }
 
 func (r *UserRepository) GetUser(ctx context.Context, req *models.ReadUserRequest) (models.User, error) {
@@ -52,76 +46,58 @@ func (r *UserRepository) GetUser(ctx context.Context, req *models.ReadUserReques
 	}
 
 	if err := r.db.Where("id = ?", req.ID).First(&user).Error; err != nil {
-		fmt.Printf("Данного пользователя не существует %v\n", err.Error())
-		return user, err
+		return user, fmt.Errorf("Repo: User not found: %w", err)
 	}
 
-	if err := r.userCaching(ctx, &user); err != nil {
-		fmt.Printf("Ошибка кэширования %v\n", err.Error())
-	}
-
-	return user, nil
+	return user, r.userCaching(ctx, &user)
 }
 
-func (r UserRepository) ReplaceUser(ctx context.Context, req *models.UpdateUserRequest) (bool, error) {
+func (r *UserRepository) ReplaceUser(ctx context.Context, req *models.UpdateUserRequest) (bool, error) {
 	result := r.db.Model(&models.User{}).Where("id = ?", req.ID).Select("*").Updates(req)
 
 	if result.Error != nil {
-		return false, result.Error
+		return false, fmt.Errorf("Repo: User not found: %w", result.Error)
 	}
 
 	if result.RowsAffected == 0 {
-		fmt.Printf("User %v not found\n", result.RowsAffected)
-		return false, fmt.Errorf("user not found")
+		return false, fmt.Errorf("Repo: User not found: %w", result.Error)
 	}
 
 	var updatedUser models.User
 	if err := r.db.Where("id = ?", req.ID).First(&updatedUser).Error; err != nil {
-		return false, err
+		return false, fmt.Errorf("Repo: Updated user not found: %w", err)
 	}
 
-	if err := r.userCaching(ctx, &updatedUser); err != nil {
-		fmt.Printf("Ошибка кэширования %v\n", err.Error())
-	}
-
-	return true, nil
+	return true, r.userCaching(ctx, &updatedUser)
 }
 
 func (r *UserRepository) PatchUser(ctx context.Context, req *models.PatchUserRequest) (bool, error) {
 	result := r.db.Where("id = ?", req.ID).Model(&models.User{}).Updates(req)
 
 	if result.Error != nil {
-		return false, result.Error
+		return false, fmt.Errorf("Repo: User not found: %w", result.Error)
 	}
 
 	if result.RowsAffected == 0 {
-		fmt.Printf("User %v not found\n", result.RowsAffected)
-		return false, fmt.Errorf("user not found")
+		return false, fmt.Errorf("Repo: User not found: %w", result.Error)
 	}
 
-	var updatedUser models.User
-	if err := r.db.Where("id = ?", req.ID).First(&updatedUser).Error; err != nil {
-		return false, err
+	var patchedUser models.User
+	if err := r.db.Where("id = ?", req.ID).First(&patchedUser).Error; err != nil {
+		return false, fmt.Errorf("Repo: Patched user not found: %w", err)
 	}
 
-	if err := r.userCaching(ctx, &updatedUser); err != nil {
-		fmt.Printf("Ошибка кэширования %v\n", err.Error())
-	}
-
-	return true, nil
+	return true, r.userCaching(ctx, &patchedUser)
 }
 
 
 func (r *UserRepository) Delete(ctx context.Context, req *models.DeleteUserRequest) (bool, error) {
 	if err := r.db.Delete(&models.User{}, req.ID).Error; err != nil {
-		fmt.Printf("Данного пользователя не существует %v\n", err.Error())
-		return false, nil
+		return false, fmt.Errorf("Repo: User not found: %w", err)
 	}
 
 	key := userCacheKey(req.ID)
-	if err := r.redisClient.Del(ctx, key).Err(); err != nil {
-		fmt.Printf("Данный пользователь не кжширован %v\n", err.Error())
-	}
+	r.redisClient.Del(ctx, key).Err()
 
 	return true, nil
 }
